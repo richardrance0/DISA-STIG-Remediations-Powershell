a57626de87e40d85bfbe4f9900aefba9a02a44a2
Richard Rance <richard.rance.it@gmail.com>
2025-05-28 13:05:04 -0500

    Create WN10-AC-000005.ps1

---
+++ WN10-AC-000005.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    This script changes the Windows 10 account lockout duration to 15 minutes or greater.
+
+.NOTES
+    Author          : Richard Rance
+    LinkedIn        : linkedin.com/in/richardrance/
+    GitHub          : github.com/richardrance0
+    Date Created    : 2025-05-28
+    Last Modified   : 2025-05-28
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AC-000005
+
+.TESTED ON
+    Date(s) Tested  : 2025-05-28
+    Tested By       : Richard Rance
+    Systems Tested  : WinRichStigIntern
+    PowerShell Ver. : 5.1.19041.5848
+
+.USAGE
+    run this in Powershell after saving the file to your computer.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# YOUR CODE GOES HERE
