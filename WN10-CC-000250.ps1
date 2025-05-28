8a1b6e76fb56a4c2a4d25a8b5a3eb8347a09f8c5
Richard Rance <richard.rance.it@gmail.com>
2025-05-28 18:53:13 -0500

    Create WN10-CC-000250.ps1
    Script that disables Cortana.

---
+++ WN10-CC-000250.ps1
@@ -0,0 +1,37 @@
+<#  Author          : Richard Rance
+    LinkedIn        : www.linkedin.com/in/richardrance
+    GitHub          : github.com/richardrance0
+    Date Created    : 2025-05-28
+    Last Modified   : 2025-05-28
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000500
+
+.TESTED ON
+    Date(s) Tested  : 2025-05-28
+    Tested By       : Richard Rance
+    Systems Tested  : Windows 10 Pro VM
+    PowerShell Ver. : 5.1.19041.5848
+
+.USAGE
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000250).ps1 
+#>
+
+# Code starts here:
+
+# Define registry path and value
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+$valueName = "AllowCortana"
+$desiredValue = 0
+
+# Create the path if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the registry value to disable Cortana
+New-ItemProperty -Path $regPath -Name $valueName -Value $desiredValue -PropertyType DWord -Force | Out-Null
+
+Write-Output "Cortana has been disabled by setting $valueName to $desiredValue at $regPath."
